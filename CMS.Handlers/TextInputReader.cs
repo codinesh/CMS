eed21c4cb9b2356cf54af91cfe8fa2eebc7b72d8
dinesh <codinesh@live.com>
2019-05-23 21:55:56 +0530

    Added exception handling.

--- CMS.Handlers/TextInputReader.cs
+++ CMS.Handlers/TextInputReader.cs
@@ -13,16 +13,33 @@ namespace CMS.Core
     {
         public async Task<IList<Talk>> LoadTalksAsync(string inputFileName)
         {
-            Regex talkDurationRegex = new Regex(@"lightning$|\d+?$");
-            //var fullPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            Regex talkDurationRegex = new Regex(@"(lightning)$|(\d+)?.{3}$");
             string[] lines = await File.ReadAllLinesAsync(inputFileName);
-
+            List<Talk> talks = new List<Talk>();
             foreach (var talk in lines)
             {
-                var duration = talkDurationRegex.Match(talk);
+                var match = talkDurationRegex.Match(talk);
+                if (match.Success)
+                {
+                    var duration = 0;
+
+                    var isLightningTalk = match.Groups[1].Success;
+                    if (isLightningTalk)
+                    {
+                        duration = 5;
+                    }
+                    else
+                    {
+                        duration = Convert.ToInt16(match.Groups[2].Value);
+                    }
+                    talks.Add(new Talk(talk, duration));
+                }
+                else {
+                    throw new InvalidInputException();
+                }
             }
 
-            return new List<Talk>();
+            return talks;
         }
     }
 }
